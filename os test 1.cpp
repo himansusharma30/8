11c6eda19bacba9fac925289ce47734d50c0cdc7
himansusharma30 <63067342+himansusharma30@users.noreply.github.com>
2020-04-03 19:23:08 +0530

    Add files via upload

---
+++ os test 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t_quantum=10;
+	int thread[]={1,2,3,4,5,6};
+	int priority[]={40,30,30,35,5,10};
+	int arrival[]={0,25,30,60,100,105};
+	int burst[]={20,25,25,15,10,10};
+	int r_burst[6]=burst[6];
+	int tat[10],wt[10];
+	
+	
+}
